ea0346cad956080cb115de1f366d4ec81c7430a6
Jack Zheng <JackZhengSit@outlook.com>
2020-12-03 14:56:34 +0800

    cross domain

---
+++ src/main/java/com/computorcenter/information/manual/config/CorsConfig.java
@@ -0,0 +1,37 @@
+package com.computorcenter.information.manual.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+// public class CorsConfig implements WebMvcConfigurer {
+//  @Override
+//  public void addCorsMappings(CorsRegistry registry) {
+//    registry
+//        .addMapping("/**")
+//        .allowedOrigins("http://localhost:8081")
+//        .allowCredentials(true)
+//        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+//        .maxAge(3600);
+//  }
+// }
+public class CorsConfig {
+  private CorsConfiguration buildconfig() {
+    CorsConfiguration corsConfiguration = new CorsConfiguration();
+    corsConfiguration.addAllowedOrigin("http://localhost:8081");
+    corsConfiguration.addAllowedHeader("*");
+    corsConfiguration.addAllowedMethod("*");
+    corsConfiguration.setAllowCredentials(true);
+    return corsConfiguration;
+  }
+
+  @Bean
+  public CorsFilter corsFilter() {
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", buildconfig());
+    return new CorsFilter(source);
+  }
+}
