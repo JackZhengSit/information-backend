a1e388d981199dff0c1ba65509460ab0ca34b0c6
Jack Zheng <JackZhengSit@outlook.com>
2020-12-09 16:57:45 +0800

    1209

--- src/main/java/com/computorcenter/information/manual/repository/BriefReportExteriorRepository.java
+++ src/main/java/com/computorcenter/information/manual/repository/BriefReportExteriorRepository.java
@@ -1,8 +1,17 @@
 package com.computorcenter.information.manual.repository;
 
 import com.computorcenter.information.manual.entity.BriefReportExterior;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 @RepositoryRestResource(path = "BriefReportExterior")
-public interface BriefReportExteriorRepository extends JpaRepository<BriefReportExterior, Long> {}
+public interface BriefReportExteriorRepository extends JpaRepository<BriefReportExterior, Long> {
+
+  //  Optional<BriefReportExterior> findByOrderNum(@Param("orderNum") Long aLong, Pageable p);
+  @RestResource(path = "findByOrderNum", rel = "findByOrderNum")
+  public Page findByOrderNum(@Param("orderNum") Long aLong, Pageable p);
+}
