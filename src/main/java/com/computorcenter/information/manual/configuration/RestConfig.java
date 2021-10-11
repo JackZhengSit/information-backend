0d802df12e1c240883de5b9ed58d75417303a649
Jack Zheng <JackZhengSit@outlook.com>
2021-10-11 13:26:20 +0800

    上传错误bugfix

--- src/main/java/com/computorcenter/information/manual/configuration/RestConfig.java
+++ src/main/java/com/computorcenter/information/manual/configuration/RestConfig.java
@@ -1,19 +1,23 @@
 package com.computorcenter.information.manual.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.stereotype.Component;
 
-// @Component
+import javax.persistence.EntityManager;
+
+@Component
 public class RestConfig implements RepositoryRestConfigurer {
-  //
-  //  @Autowired EntityManager entityManager;
-  //
-  //  @Override
-  //  public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
-  //
-  //    config.exposeIdsFor(
-  //        entityManager.getMetamodel().getEntities().stream()
-  //            .map(entityType -> entityType.getJavaType())
-  //            .toArray(Class[]::new));
-  //  }
+
+  @Autowired EntityManager entityManager;
+
+  @Override
+  public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+
+    config.exposeIdsFor(
+        entityManager.getMetamodel().getEntities().stream()
+            .map(entityType -> entityType.getJavaType())
+            .toArray(Class[]::new));
+  }
 }
